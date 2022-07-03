778b0cf9d90ef578f55a62423f774b6b2e5fc3cd
Xanarey <doctor.layout@mail.ru>
2022-07-03 14:50:06 +0300

    tests

---
+++ src/test/java/com/tim/service/DeveloperServiceTest.java
@@ -0,0 +1,28 @@
+package com.tim.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeveloperServiceTest {
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void insert() {
+    }
+}

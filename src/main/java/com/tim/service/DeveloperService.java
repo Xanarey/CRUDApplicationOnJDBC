8bc4179f1e07af7bdd30dc81a89f18b934ef5fd9
Xanarey <doctor.layout@mail.ru>
2022-07-05 19:24:25 +0300

    new_db

--- src/main/java/com/tim/service/DeveloperService.java
+++ src/main/java/com/tim/service/DeveloperService.java
@@ -17,7 +17,7 @@ public class DeveloperService implements DeveloperRepository {
     public DeveloperService() throws SQLException {
     }
 
-    @Override
+    @Override                                             // TODO утечка ресурсов!
     public void save() throws SQLException {
         if (resultSet != null) resultSet.close();
         if (statement != null) statement.close();
@@ -27,31 +27,31 @@ public class DeveloperService implements DeveloperRepository {
 
     @Override
     public void getAll() throws SQLException {
-        sqlQuery = "SELECT * FROM developers";
+        sqlQuery = "SELECT firstName, lastName, status, specialty.name AS specialty_name, skills.name AS skill_name " +
+                "FROM developer, specialty, skills";
         resultSet = statement.executeQuery(sqlQuery);
         System.out.println("\nDevelopers:");
         while (resultSet.next()) {
             int id = resultSet.getInt("id");
-            String FirstName = resultSet.getString("FirstName");
-            String LastName = resultSet.getString("LastName");
+            String FirstName = resultSet.getString("firstName");
+            String LastName = resultSet.getString("lastName");
             String status = resultSet.getString("status");
-            String specialty = resultSet.getString("specialty");
-            String skills = resultSet.getString("skills");
+            String specialty_name = resultSet.getString("specialty_name");
+            String skill_name = resultSet.getString("skill_name");
 
             System.out.println("\n================\n");
-            System.out.println("developers_id: " + id);
-            System.out.println("FirstName: " + FirstName);
-            System.out.println("LastName: " + LastName);
+            System.out.println("id: " + id);
+            System.out.println("firstName: " + FirstName);
+            System.out.println("lastName: " + LastName);
             System.out.println("status: " + status);
-            System.out.println("specialty: " + specialty);
-            System.out.println("skills: " + skills);
+            System.out.println("specialty_name: " + specialty_name);
+            System.out.println("skill_name: " + skill_name);
         }
     }
 
     @Override
     public void deleteById(Long id) throws SQLException {
-        sqlQuery = "DELETE FROM developers " +
-                   "WHERE id = ?";
+        sqlQuery = "DELETE FROM developer WHERE id = ?";
         preparedStatement = connection.prepareStatement(sqlQuery);
         preparedStatement.setLong(1, id);
         preparedStatement.executeUpdate();
@@ -59,9 +59,7 @@ public class DeveloperService implements DeveloperRepository {
 
     @Override
     public void update(Long id, String firstNewName, String lastNewName) throws SQLException {
-        sqlQuery = "UPDATE developers " +
-                   "SET FirstName = ?, LastName = ?" +
-                   "WHERE id = ?";
+        sqlQuery = "UPDATE developer SET firstName = ?, lastName = ? WHERE id = ?";
         preparedStatement = connection.prepareStatement(sqlQuery);
         preparedStatement.setString(1 ,firstNewName);
         preparedStatement.setString(2 ,lastNewName);
@@ -71,7 +69,7 @@ public class DeveloperService implements DeveloperRepository {
 
     @Override
     public void insert(String firstName, String lastName, String status, String specialty, String skills) throws SQLException {
-        sqlQuery = "INSERT INTO developers(id, FirstName, LastName, Status, Specialty, Skills) " +
+        sqlQuery = "INSERT INTO developer(id, firstName, lastName, status) " +
                    "VALUES (id, ?,?,?,?,?)";
 
         preparedStatement = connection.prepareStatement(sqlQuery);
